086125973e5bd4a116146e4ebe9ad4b794cfb2df
Usama Khalid <osamakhalidgoraya@gmail.com>
2018-04-26 18:55:11 +0500

    ES6 function call syntax

--- src/Note.js
+++ src/Note.js
@@ -4,55 +4,55 @@ import FaTrash from 'react-icons/lib/fa/trash'
 import FaFloppyO from 'react-icons/lib/fa/floppy-o'
 
 class Note extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            editing: false
-        }
-    }
-    edit = () => {
-        this.setState({
-            editing: true
-        })
-    }
+	constructor(props) {
+		super(props)
+		this.state = {
+			editing: false
+		}
+	}
+	edit = () => {
+		this.setState({
+			editing: true
+		})
+	}
 
-    remove = () => {
-        alert('removing note')
-    }
+	remove = () => {
+		this.props.onRemove(this.props.index)
+	}
 
-    save = (e) =>  {
-        e.preventDefault;
-        this.props.onChange(this._newText.value,this.props.index)
-        this.setState({
-            editing: false
-        })
-    }
+	save = (e) => {
+		e.preventDefault()
+		this.props.onChange(this._newText.value, this.props.index)
+		this.setState({
+			editing: false
+		})
+	}
 
-    renderForm = () => {
-        return (
-            <div className="note">
-                <form onSubmit={this.save}>
-                    <textarea ref={input => this._newText = input}/>
-                    <button><FaFloppyO /></button>
-                </form>
-            </div>
-        )
-    }
+	renderForm = () => {
+		return (
+			<div className="note">
+				<form onSubmit={this.save}>
+					<textarea ref={input => this._newText = input}/>
+					<button id="save"><FaFloppyO /></button>
+				</form>
+			</div>
+		)
+	}
 
-    renderDisplay = () => {
-        return (
-            <div className="note">
-                <p>{this.props.children}</p>
-                <span>
+	renderDisplay = () => {
+		return (
+			<div className="note">
+				<p>{this.props.children}</p>
+				<span>
 					<button onClick={this.edit} id="edit"><FaPencil /></button>
 					<button onClick={this.remove} id="remove"><FaTrash /></button>
 				</span>
-            </div>
-        )
-    }
-    render() {
-        return this.state.editing ? this.renderForm() : this.renderDisplay()
-    }
+			</div>
+		)
+	}
+	render() {
+		return this.state.editing ? this.renderForm() : this.renderDisplay()
+	}
 
 }
 
